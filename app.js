1609a3dd8801407dfe91ad70bc59ff7fff0f62be
emretask1n <64936096+emretask1n@users.noreply.github.com>
2022-09-18 17:19:46 +0300

    Custom List Possibility added and Connected With MongoDB

--- app.js
+++ app.js
@@ -1,45 +1,150 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(express.static("public"));
 
-const items = ["Buy Food","Cook Food","Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", itemSchema);
+
+const welcomeItem = new Item({
+  name: "Welcome to your to do list! 🤩"
+});
+
+const addNewItem = new Item({
+  name: "Hit the + button to add a new item ! 🆕"
+});
+
+const deleteItem = new Item({
+  name: "Hit checkbox to add delete an item ! ✅"
+});
+
+const defaultItems = [welcomeItem, addNewItem, deleteItem];
+
+const listSchema = {
+  name: String,
+  items: [itemSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
 
-  let day = date.getDate();
+  Item.find({}, function(err, foundItems) {
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Success");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {
+        listTitle: "Today",
+        newListItems: foundItems
+      });
+    }
+  });
 
-  res.render("list", {listTitle:day, newListItems:items});
 });
 
-app.post("/", function(req, res){
-  let item = req.body.newItem;
+app.get("/:customListName", function(req, res) {
+  const customListName = _.capitalize(req.params.customListName);
 
-  if ( req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
+  List.findOne({name: customListName}, function(err, foundList) {
+      if (!err) {
+        if (!foundList) {
+          const list = new List({
+            name: customListName,
+            items: defaultItems
+          });
+          list.save();
+          res.redirect("/" + customListName);
+        } else {
+          res.render("list", {listTitle: foundList.name, newListItems: foundList.items});
+        }
+      }
+  });
+});
+
+
+app.post("/", function(req, res) {
+  const itemName = req.body.newItem;
+  const listName = req.body.list;
+
+  const item = new Item({
+    name: itemName
+  });
+
+  if (listName === "Today") {
+    item.save();
     res.redirect("/");
+  } else {
+    List.findOne({
+      name: listName
+    }, function(err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      res.redirect("/" + listName);
+    })
   }
+
+});
+
+app.post("/delete", function(req, res) {
+  const checkedItemId = req.body.checkbox;
+  const listName = req.body.listName;
+
+  if (listName === "Today") {
+    Item.findByIdAndRemove(checkedItemId, function(err) {
+      if (!err) {
+        console.log("Item succesfully deleted.");
+        res.redirect("/");
+      }
+    });
+  } else {
+    List.findOneAndUpdate({
+      name: listName
+    }, {
+      $pull: {
+        items: {
+          _id: checkedItemId
+        }
+      }
+    }, function(err, foundList) {
+      if (!err) {
+        res.redirect("/" + listName);
+      }
+    });
+  }
+
 });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000.");
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
-});
 
 
-app.post("/work", function(req,res){
+
+app.post("/work", function(req, res) {
   let item = res.body.newItem;
   workItems.push(item);
   res.redirect("/work");
